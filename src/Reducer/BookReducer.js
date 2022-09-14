ad5915c693536d95cb546a0bdb2548c4fb761af8
PrgDominique <dominiquesimbillo10@gmail.com>
2022-09-14 18:53:45 +0800

    initial commit

---
+++ src/Reducer/BookReducer.js
@@ -0,0 +1,18 @@
+import React,{ useReducer} from 'react'
+import {v1 as uuid} from 'uuid'
+
+export const BookReducer = (state, action) => {
+
+    switch(action.type){
+        case 'ADD_BOOK':
+        return [...state, {
+            title: action.book.title,
+            author: action.book.author,
+            id: uuid()
+        }]
+        case 'REMOVE_BOOK':
+            return state.filter(book => book.id !== action.id)
+            default: 
+            return state
+        }
+}
---
+++ src/context/BookContext.js
@@ -0,0 +1,28 @@
+import React,{createContext, useEffect, useReducer} from 'react'
+
+import { BookReducer } from '../Reducer/BookReducer'
+
+export const BookContext = createContext()
+
+const BookContextProvider = (props) => {
+    
+   const [books, dispatch] = useReducer(BookReducer, [], () => {
+    const localData = localStorage.getItem('books')
+    return localData ? JSON.parse(localData) : []
+    })
+
+   useEffect(()=> {
+    localStorage.setItem('books', JSON.stringify(books))
+   }, [books])
+
+
+   
+    return ( 
+        <BookContext.Provider value={{books, dispatch}}>
+           { props.children}
+        </BookContext.Provider>
+
+     );
+}
+ 
+export default BookContextProvider
